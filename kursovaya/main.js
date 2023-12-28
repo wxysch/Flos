ce5c205e8e518049a8dd63b0d3fde07aa3ea8b04
wxysch <uwu1343@gmail.com>
2023-12-28 07:20:50 +0600

    burger menu

--- kursovaya/main.js
+++ kursovaya/main.js
@@ -1,3 +1,19 @@
+let btn = document.querySelector(".mobile-btn");
+
+const burgerFunc = (menuClassName, showClassName) => {
+  let menu = document.querySelector(`.${menuClassName}`);
+
+  if (menu.className.includes(showClassName)) {
+    menu.className = menuClassName;
+  } else {
+    menu.className += ` ${showClassName}`;
+  }
+};
+
+btn.addEventListener("click", () => {
+  burgerFunc("header__menu", "header__menu-show");
+});
+
 const swiper_thumbnail = new Swiper(".swiper_thumbnail", {
   slidesPerView: 3,
 });
